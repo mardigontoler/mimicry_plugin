6d1ad3900861c0195ba8bb387c2dc5833804428c
mtoler <mardi.emerald@gmail.com>
2025-04-21 21:38:42 -0400

    fix deprecated font construction

--- src/gui/font.h
+++ src/gui/font.h
@@ -6,23 +6,23 @@
 #include "juce_gui_basics/juce_gui_basics.h"
 
 namespace mimicry {
-    static juce::Font getSegmentFont(int heigth) {
+    static juce::Font getSegmentFont(float heigth) {
         static auto typeface = juce::Typeface::createSystemTypefaceFor(BinaryData::Segment74Gml_otf, BinaryData::Segment74Gml_otfSize);
-        juce::Font font(typeface);
+        juce::Font font((juce::FontOptions(typeface)));
         font.setHeight(heigth);
         return font;
     }
 
-    static juce::Font getHeaderFont(int height) {
+    static juce::Font getHeaderFont(float height) {
         static auto typeface = juce::Typeface::createSystemTypefaceFor(BinaryData::CinzelDecorativeRegular_ttf, BinaryData::CinzelDecorativeRegular_ttfSize);
-        juce::Font font(typeface);
+		juce::Font font((juce::FontOptions(typeface)));
         font.setHeight(height);
         return font;
     }
 
-    static juce::Font getNormalFont(int height) {
+    static juce::Font getNormalFont(float height) {
         static auto typeface = juce::Typeface::createSystemTypefaceFor(BinaryData::UbuntuMedium_ttf, BinaryData::UbuntuMedium_ttfSize);
-        juce::Font font(typeface);
+		juce::Font font((juce::FontOptions(typeface)));
         font.setHeight(height);
         return font;
     }
